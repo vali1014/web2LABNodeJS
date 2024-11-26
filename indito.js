7542c1d552857aa38053a62e728f7a162ed6f51a
valisecond <valcseszka1369@gmail.com>
2024-11-27 00:03:21 +0100

    létrehoztam a menüt és a hozzájuk tartozó oldalakat

--- indito.js
+++ indito.js
@@ -1,12 +1,26 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = 8012;
 
-app.use(express.static('public'));
+const adatbazisRouter = require('./public/adatbazis');
+const kapcsolatRouter = require('./public/kapcsolat');
+const uzenetekRouter = require('./public/uzenetek');
+const crudRouter = require('./public/crud');
+const oopRouter = require('./public/oop');
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/style.css', express.static(path.join(__dirname, 'style.css')));
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/public/index.html');
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
+
+app.use('/adatbazis', adatbazisRouter);
+app.use('/kapcsolat', kapcsolatRouter);
+app.use('/uzenetek', uzenetekRouter);
+app.use('/crud', crudRouter);
+app.use('/oop', oopRouter);
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
---
+++ public/uzenetek.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  res.send(`
+    <!DOCTYPE html>
+    <html lang="hu">
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Üzenetek</title>
+      <link rel="stylesheet" href="/style.css">
+    </head>
+    <body>
+      <nav>
+        <a href="/">Főoldal</a>
+        <a href="/adatbazis">Adatbázis</a>
+        <a href="/kapcsolat">Kapcsolat</a>
+        <a href="/uzenetek">Üzenetek</a>
+        <a href="/crud">CRUD</a>
+        <a href="/oop">OOP</a>
+      </nav>
+      <main>
+        <h1>Üzenetek oldal</h1>
+      </main>
+    </body>
+    </html>
+  `);
+});
+
+module.exports = router;
